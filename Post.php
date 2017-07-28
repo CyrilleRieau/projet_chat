c2d534b2aeeb36e0c394554edd5b415d7356a607
Cyrille <cyrille.rieau@gmail.com>
2017-07-28 17:18:46 +0200

    Loll De la merde

--- Post.php
+++ Post.php
@@ -7,9 +7,43 @@ class Post {
     protected $id;
 
     function __construct(string $contenu, $date, string $auteur, int $id = NULL) {
-         $this->contenu = $contenu;
+        $this->contenu = $contenu;
         $this->date = $date;
         $this->auteur = $auteur;
         $this->id = $id;
     }
-    
+
+    function getContenu() {
+        return $this->contenu;
+    }
+
+    function getDate() {
+        return $this->date;
+    }
+
+    function getAuteur() {
+        return $this->auteur;
+    }
+
+    function getId() {
+        return $this->id;
+    }
+
+    function setContenu($contenu) {
+        $this->contenu = $contenu;
+    }
+
+    function setDate($date) {
+        $this->date = $date;
+    }
+
+    function setAuteur($auteur) {
+        $this->auteur = $auteur;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+
+    }    
--- chat.php
+++ chat.php
@@ -1,5 +1,25 @@
 <?php
-if(isset($_GET['text'])){
-    echo($_GET['text']);
-    }
+
+include_once './Post.php';
+
+$obj = [];
+if (empty($_POST['text'])) {
+http_response_code(400);
+header('Content-TYpe: text/plain');
+echo 'expect a message parameter';
+exit(1);
+}
+echo($_POST['text']);
+/* if(isset($_GET['text'])){
+  echo($_GET['text']);
+  } */
+$date = new DateTime();
+$date->format('Y-m-d H:i:s');
+$post = new Post($_POST['text'], $date, 'Bezu');
+//$posts = new Post($_POST['text'], $date, 'Bezu');
+//$serpost = json_encode($post);
+array_push($obj, $post);
+//array_push($obj, $posts);
+$tabl = json_encode($obj);
+
 ?>
