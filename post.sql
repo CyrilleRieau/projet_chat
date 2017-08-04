50cc4fdfa0d37e063e3b3d2279c28733024768fb
Cyrille <cyrille.rieau@gmail.com>
2017-08-04 14:17:23 +0200

    Test

--- post.sql
+++ post.sql
@@ -2,9 +2,7 @@ DROP DATABASE IF EXISTS `cyrille_posts_db`;
 
 CREATE DATABASE `cyrille_posts_db` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-GRANT ALL PRIVILEGES ON `cyrille_posts_db`.* TO 'ajax-chat-user'@'localhost' IDENTIFIED BY 'We Love SQL API!';
-
-USE `posts_db`;
+USE `cyrille_posts_db`;
 
 CREATE TABLE `posts` (
     `id`        INT AUTO_INCREMENT PRIMARY KEY,
